cb8a01723c046743ed1760e4eb3ef5b44a397220
Polianskyi Dmytro <dimdemon1@gmail.com>
2025-03-22 16:06:40 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const list = [...document.querySelectorAll('li')];
+
+function convertToNumber(str) {
+  return Number(str.split('$').join('').split(',').join(''));
+}
+
+function sortList(people) {
+  return people.sort(
+    (a, b) =>
+      convertToNumber(b.dataset.salary) - convertToNumber(a.dataset.salary),
+  );
+}
+
+function getEmployees(people) {
+  for (const person of people) {
+    document.querySelector('ul').appendChild(person);
+  }
+}
+
+getEmployees(sortList(list));
